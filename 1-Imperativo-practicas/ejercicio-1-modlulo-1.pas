8a26dc2be43902136b426da5d00fad19b50f8647
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-08-02 14:03:04 -0300

    continuar ejercicio1-modulo1

---
+++ 1-Imperativo-practicas/ejercicio-1-modlulo-1.pas
@@ -0,0 +1,54 @@
+program ej1Mod1;
+
+uses
+  sysUtils;
+
+const
+  dimF = 20;
+
+type
+
+  rVentas = 1..dimF;
+  rCodP = 0..15;
+  rCantProd = 1..99;
+
+  venta = record
+    codP: rCodP;
+    cantProd: rCantProd;
+  end;
+
+  vVentas = array[rVentas] of venta;
+
+{modulos}
+
+procedure leerVenta(var v: venta);
+begin
+  with v do begin
+    codP := Random(rCodP);
+    if (codP <> 0) then begin  
+      write('Ingrese cantidad de unidades vendidas');
+      readln(cantProd);
+    end;
+  end;
+end;
+
+procedure carcarVector(var v: vVentas; var dimL: integer;);
+var
+  v: venta;
+begin
+  leerVenta(v);
+  while (dimL <= dimF) and (v.codP <> 0) do begin
+    dimL := dimL + 1;
+    v[dimL] := v;
+  end;
+end;
+
+{Programa principal}
+var
+  v: vVentas;
+  dimL: integer;
+begin
+  Randomize;
+  dimL := 0;
+  carcarVector(v,dimL);
+end.
