2dd76cf66146b23b450384be5679ce6d9c394aa1
Agustin Firpo <agustin_@outlook.com.ar>
2024-08-27 00:33:07 -0300

    ejercicio 3 completo

--- 1-Imperativo-practicas/practica 1/ejercicio-3-P1.pas
+++ 1-Imperativo-practicas/practica 1/ejercicio-3-P1.pas
@@ -1,91 +1,213 @@
-program ejercicio3P1;
 
-Const
+Program ejercicio3P1;
+
+Const 
   dimF_gen = 8;
 
-type
-  rGen: 1..dimF_gen;
+Type 
+  rGen  = 1..dimF_gen;
 
   pelicula = Record
     cod: integer;
     gen: rGen;
     cal: real;
-  end;
+  End;
 
   lPeliculas = ^nPeliculas;
 
-  nPeliculas = record
+  nPeliculas = Record
     dato: pelicula;
     sig: lPeliculas;
-  end;
+  End;
 
-  lista = record
+  maximos = Record
+    codMax: integer;
+    punMax: real;
+  End;
+
+  lista = Record
     pri: lPeliculas;
     ult: lPeliculas;
-  end;
+  End;
 
-  vGeneneros = array[rGen] of lista;
-
+  vGeneneros = array[rGen] Of lista;
+  vMax = array[rGen] Of maximos;
 
 {modulos}
 
-  procedure leerPelicula(var p: pelicula);
-  begin
-    with p do begin
-      cod := Random(102) - 1;
-      gen := Random(dimF_gen) + 1;
-      cal := Random(10) + 1;
-    end;
-  end;
+Procedure leerPelicula(Var p: pelicula);
+Begin
+  With p Do
+    Begin
+      cod := Random(22) - 1;
+      If cod <> -1 Then
+        Begin
+          gen := Random(dimF_gen) + 1;
+          cal := Random(10) + 1;
+        End;
+    End;
+End;
 
-  procedure agregarAtras(var l: lista; p: pelicula);
-  var
-    nue: lPeliculas;
-  begin
-    new(nue);
-    nue^.dato := p;
-    if l.pri = nil then
-      l.pri := nue
-    else
-      l.ult^.sig := nue;
-    l.ult := nue;
-  end;
+Procedure agregarAtras(Var l: lista; p: pelicula);
 
-  procedure generarLista(var l: lista);
-  var
-    p: pelicula;
-  begin
-    leerPelicula(p);
-    while (p.cod <> -1) do begin
-      agregarAtras(l,p);
+Var 
+  nue: lPeliculas;
+Begin
+  new(nue);
+  nue^.dato := p;
+  nue^.sig := Nil;
+  If (l.pri = Nil) Then
+    l.pri := nue
+  Else
+    l.ult^.sig := nue;
+  l.ult := nue;
+End;
+
+Procedure cargarVector(Var v:vGeneneros);
+
+Var 
+  p: pelicula;
+Begin
+  leerPelicula(p);
+  While (p.cod <> -1) Do
+    Begin
+      agregarAtras(v[p.gen],p);
       leerPelicula(p);
-    end;
-  end;
+    End;
+End;
 
-  procedure inicializarVector(var v: vGeneneros);
-  var
-    i: rGen;
-  begin
-    for i := 1 to dimF_gen do
+Procedure inicializarVector(Var v: vGeneneros);
+
+Var 
+  i: rGen;
+Begin
+  For i := 1 To dimF_gen Do
+    Begin
       v[i].pri := Nil;
       v[i].ult := Nil;
-  end;
+    End;
+End;
 
-  procedure cargarVector(var v:vGeneneros);
-  var
-    i: rGen;
-    p: pelicula;
-  begin
-    leerPelicula(p);
-    while
-  end;
+Procedure inicializarMaximos(Var v: vMax);
+
+Var 
+  i: rGen;
+Begin
+  For i := 1 To dimF_gen Do
+    Begin
+      v[i].punMax := -1;
+      v[i].codMax := -1;
+    End;
+End;
+
+Procedure imprimirVector(v: vGeneneros);
+
+Var 
+  i: rGen;
+  aux: lPeliculas;
+Begin
+  For i := 1 To dimF_gen Do
+    Begin
+      aux := v[i].pri;
+      While (aux <> Nil) Do
+        Begin
+          writeln();
+          write('Codigo de pelicula: ', aux^.dato.cod);
+          writeln();
+          write('Codigo de genero: ', aux^.dato.gen);
+          writeln();
+          write('Calificación de la pelicula: ', aux^.dato.cal);
+          aux := aux^.sig;  { Avanzar en la lista }
+        End;
+    End;
+End;
+
+Procedure maxPeliculas(v: vGeneneros; Var vM: vMax);
+
+Var 
+  i: integer;
+  act: lPeliculas;
+Begin
+  For i := 1 To dimF_gen Do
+    Begin
+      act := v[i].pri;
+      While (act <> Nil) Do
+        Begin
+          If (act^.dato.cal > vM[i].punMax) Then
+            Begin
+              vM[i].punMax := act^.dato.cal;
+              vM[i].codMax := act^.dato.cod;
+            End;
+          act := act^.sig;
+        End;
+    End;
+End;
+
+Procedure ordenacionInsercion (Var v: vMax);
+
+Var 
+  j, i: integer;
+  aux: maximos;
+Begin
+  For i:= 2 To dimF_gen Do
+    Begin
+      aux := v[i];
+      j := i-1;
+      While (j > 0) And (v[j].punMax > aux.punMax) Do
+        Begin
+          v[j+1] := v[j];
+          j := j -1;
+        End;
+      v[j+1] := aux;
+    End;
+End;
+
+Procedure mostrarVecMax(Var v: vMax);
+
+Var 
+  i: integer;
+Begin
+  For i:= dimF_gen Downto 1 Do
+    Begin
+      If (i = dimF_gen)Then
+        writeln('El codigo de pelicula con mayor puntaje es: ',v[i].codMax);
+      If (i = 1)Then
+        writeln('El codigo de pelicula con menor puntaje es: ',v[i].codMax);
+    End;
+End;
+
+Procedure imprimirVector(v: vGeneneros);
+
+Var 
+  i: rGen;
+  act: lPeliculas;
+Begin
+  For i := 1 To dimF_gen Do
+    Begin
+      act := v[i].pri;
+      While act <> Nil Do
+        Begin
+
+        End;
+    End;
+End;
+
 
 {Programa principal}
-var
+
+Var 
   v: vGeneneros;
-begin
+  vM: vMax;
+Begin
   Randomize;
+
   inicializarVector(v);
   cargarVector(v);
-  
-end.
+  imprimirVector(v);
+
+  inicializarMaximos(vM);
+  maxPeliculas(v,vM);
+
+  ordenacionInsercion(vM);
+  mostrarVecMax(vM);
+End.
