b28104d2cfa0cdf1586b5b2c38cb747a8c7dbd15
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-09-23 11:27:34 -0300

    .

--- 2-Poo-practicas/ProyectoAgustinFirpo/src/practica 3/Ejercicio1P3_AF.java
+++ 2-Poo-practicas/ProyectoAgustinFirpo/src/practica 3/Ejercicio1P3_AF.java
@@ -1,7 +1,25 @@
 public class Main {
+
+  import PaqueteLectura.Lector;
   
   public static void main(String args[]) {
-    triangulo t = new triangulo(12,12,12,"rojo","verde");
+
+    //declaro objeto triangulo y lo creo vacio.
+    triangulo t = new triangulo();
+
+    //Cargo los valores del triagulo.
+    System.out.print("Ingrese medida del lado 1: ");
+    t.setLado1(Lector.leerInt());
+    System.out.print("Ingrese medida del lado 2: ");
+    t.setLado2(Lector.leerInt());
+    System.out.print("Ingrese medida del lado 3: ");
+    t.setLado3(Lector.leerInt());
+    System.out.print("Ingrese el color de relleno: ");
+    t.setColorRelleno(Lector.leerString());
+    System.out.print("Ingrese el color de linea: ");
+    t.setColorLinea(Lector.leerString());
+
+    //Imprimir valores del objeto triangulo
     System.out.println(t.toString());
     System.out.println("El perimetro del triangulo es: " + t.calcularPerimetro());
     System.out.printf("El Area del triangulo es: %.2f\n", t.calcularArea());
