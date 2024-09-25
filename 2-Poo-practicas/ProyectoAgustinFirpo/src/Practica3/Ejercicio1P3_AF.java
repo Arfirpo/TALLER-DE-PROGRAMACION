9313c16a0976412a865f9786849f0290e77338d5
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-09-25 12:40:13 -0300

    arreglos

--- 2-Poo-practicas/ProyectoAgustinFirpo/src/Practica3/Ejercicio1P3_AF.java
+++ 2-Poo-practicas/ProyectoAgustinFirpo/src/Practica3/Ejercicio1P3_AF.java
@@ -1,3 +1,14 @@
+/*
+1-A- Definir una clase para representar triángulos. Un triángulo se caracteriza por el tamaño de sus 3 lados (double), el color de relleno (String) y el color de línea (String).
+Provea un constructor que reciba todos los datos necesarios para iniciar el objeto.
+Provea métodos para:
+- Devolver/modificar el valor de cada uno de sus atributos (métodos get y set)
+- Calcular el perímetro y devolverlo (método calcularPerimetro)
+- Calcular el área y devolverla (método calcularArea)
+B- Realizar un programa que instancie un triángulo, le cargue información leída desde
+teclado e informe en consola el perímetro y el área.
+*/
+
 package Practica3;
 
 import PaqueteLectura.Lector;
@@ -23,7 +34,7 @@ public class Ejercicio1P3_AF {
 
     //Imprimir valores del objeto triangulo
     System.out.println(t.toString());
-    System.out.println("El perimetro del triangulo es: " + t.calcularPerimetro());
+    System.out.println("El perimetro del triangulo es: %.2f\n" + t.calcularPerimetro());
     System.out.printf("El Area del triangulo es: %.2f\n", t.calcularArea());
   }
 }
